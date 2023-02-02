8764dc4ae11b1f6b43eeae1c3723001101970ee3
UnderSaga <undersaga@mail.ru>
2023-02-02 18:51:48 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variant9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первое число: ");
+            string first_num = Console.ReadLine();
+            Console.Write("Введите второе число: ");
+            string second_num = Console.ReadLine();
+            Console.WriteLine("Вывожу первое число: " + first_num);
+            Console.WriteLine("Вывожу второе число: " + second_num);
+        }
+    }
+}
